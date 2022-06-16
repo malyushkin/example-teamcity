178debb4d22ac13b540b921cc4c8bc2c208a028c
Roman <malyushkinr@gmail.com>
2022-06-16 12:28:15 +0200

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -26,4 +26,8 @@ public class WelcomerTest {
 	public void welcomerSaysSilver(){
 		assertThat(welcomer.sayNeedGold(), containsString("gold"));
 	}
+	@Test
+	public void welcomerSaysHunter(){
+		assertThat(welcomer.sayHunter(), containsString("hunter"));
+	}
 }
